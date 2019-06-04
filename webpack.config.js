f9ff4933d3ed968c99f2240fe5a2e34c6e3882b6
edilovaysky <edilovaysky@mail.ru>
2019-06-04 17:15:43 +0800

    add hash to file bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'bundle[chunkhash].js',
   },
   module: {
     rules: [
